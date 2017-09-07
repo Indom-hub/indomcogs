e7b108c2bd47b61a4c09dd2ccb73d15150fe6119
IllusiveTea <31740751+IllusiveTea@users.noreply.github.com>
2017-09-07 19:03:29 +0100

    Add files via upload

---
+++ servercheck.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+	"""IllusiveTea's Custom cog that checks if a server is up!"""
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def isserverup(self):
+		"""Checks if a server is up"""
+
+		import socket;
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		result = sock.connect_ex(("PUT SERVER IP HERE", PORT HERE-30120 is default for FiveM ))
+		if result == 0:
+			await self.bot.say("Server is Currently: __**Online!**__")
+		else:
+			await self.bot.say("Server is Currently: __**Offline**__ :frowning:.")
+
+
+def setup(bot):
+	bot.add_cog(Mycog(bot))
