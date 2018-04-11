5d3b4a9450dfdafb0fd39cc659e2ba3c07863761
Indominus121 <tmsadler104@mail.cfcc.edu>
2018-04-11 09:16:08 -0400

    Update servercheck.py
    Added IP and added development server

--- servercheck.py
+++ servercheck.py
@@ -8,12 +8,24 @@ class Mycog:
 		self.bot = bot
 
 	@commands.command()
-	async def ss(self):
+	async def status public(self):
 		"""Checks if a server is up"""
 
 		import socket;
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		result = sock.connect_ex(("PUT SERVER IP HERE", PORT HERE-30120 is default for FiveM ))
+		result = sock.connect_ex(("158.69.250.106", 30120-30120 is default for FiveM ))
+		if result == 0:
+			await self.bot.say("Server is Currently: __**Online!**__")
+		else:
+			await self.bot.say("Server is Currently: __**Offline**__ :frowning:.")
+			
+	@commands.command()
+	async def status developement(self):
+		"""Checks if a server is up"""
+
+		import socket;
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		result = sock.connect_ex(("158.69.250.106", 30120-30120 is default for FiveM ))
 		if result == 0:
 			await self.bot.say("Server is Currently: __**Online!**__")
 		else:
