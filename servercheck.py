dc861b09bada8f5144ff16da92605622ef283cfa
cygnus <tmsadler104@mail.cfcc.edu>
2019-11-20 17:43:33 -0500

    Update servercheck.py

--- servercheck.py
+++ servercheck.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class Mycog:
-	"""IllusiveTea's Custom cog that checks if a server is up!"""
+	"""Server Checker ~wew~"""
 
 	def __init__(self, bot):
 		self.bot = bot
@@ -13,11 +13,11 @@ class Mycog:
 
 		import socket;
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		result = sock.connect_ex(("158.69.250.106", 30120-30120 is default for FiveM ))
+		result = sock.connect_ex(("77.73.70.126", 30120-30120 is default for FiveM ))
 		if result == 0:
-			await self.bot.say("Server is Currently: __**Online!**__")
+			await self.bot.say("Development Server is Currently: __**Online!**__ :partyingface:")
 		else:
-			await self.bot.say("Server is Currently: __**Offline**__ :frowning:.")
+			await self.bot.say("Development Server is Currently: __**Offline**__ :loudlycryingface:")
 			
 	@commands.command()
 	async def status developement(self):
@@ -25,11 +25,11 @@ class Mycog:
 
 		import socket;
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		result = sock.connect_ex(("158.69.250.106", 30120-30120 is default for FiveM ))
+		result = sock.connect_ex(("77.73.70.126", 30120-30120 is default for FiveM ))
 		if result == 0:
-			await self.bot.say("Server is Currently: __**Online!**__")
+			await self.bot.say("Server is Currently: __**Online!**__ :partyingface:")
 		else:
-			await self.bot.say("Server is Currently: __**Offline**__ :frowning:.")
+			await self.bot.say("Server is Currently: __**Offline**__ :loudlycryingface:")
 
 
 def setup(bot):
