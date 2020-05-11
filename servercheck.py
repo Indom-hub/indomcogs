bbf301e743b6984aea2e2e5758a53889ee2e1734
Indominus <the_tylerm@yahoo.com>
2020-05-11 18:59:44 -0400

    Update servercheck.py

--- servercheck.py
+++ servercheck.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class Mycog:
-	"""Server Checker ~wew~"""
+	"""Server Checker"""
 
 	def __init__(self, bot):
 		self.bot = bot
@@ -13,11 +13,11 @@ class Mycog:
 
 		import socket;
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		result = sock.connect_ex(("77.73.70.126", 30120-30120 is default for FiveM ))
+		result = sock.connect_ex(("77.73.68.182", 30120-30120 is default for FiveM ))
 		if result == 0:
-			await self.bot.say("Development Server is Currently: __**Online!**__ :partyingface:")
+			await self.bot.say("The Pre-Alpha Server is currently: __**Online!**__ :partyingface:")
 		else:
-			await self.bot.say("Development Server is Currently: __**Offline**__ :loudlycryingface:")
+			await self.bot.say("The Pre-Alpha Server is Currently: __**Offline**__ :loudlycryingface:")
 			
 	@commands.command()
 	async def status developement(self):
@@ -25,11 +25,11 @@ class Mycog:
 
 		import socket;
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		result = sock.connect_ex(("77.73.70.126", 30120-30120 is default for FiveM ))
+		result = sock.connect_ex(("77.73.68.182", 30100-30120 is default for FiveM ))
 		if result == 0:
-			await self.bot.say("Server is Currently: __**Online!**__ :partyingface:")
+			await self.bot.say("The Pre-Alpha Development Server is Currently: __**Online!**__ :partyingface:")
 		else:
-			await self.bot.say("Server is Currently: __**Offline**__ :loudlycryingface:")
+			await self.bot.say("The Pre-Alpha Development Server is Currently: __**Offline**__ :loudlycryingface:")
 
 
 def setup(bot):
